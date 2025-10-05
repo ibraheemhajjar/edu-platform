26a24d62c4dd610e9cd7f6b944d1396290715401
ibrahim hajjar <ibraheemhajjar91@gmail.com>
2025-10-05 11:40:57 +0300

    feat: add course sync endpoint for medusa.js integration

--- apps/backend/src/course/course.controller.ts
+++ apps/backend/src/course/course.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto, UpdateCourseDto, CourseIdDto } from './course.dto';
+import { StudentIdDto } from '../student/student.dto';
 
 @Controller('courses')
 export class CourseController {
@@ -16,6 +17,11 @@ export class CourseController {
     return this.courseService.findAll();
   }
 
+  @Get('sync')
+  syncToMedusa() {
+    return this.courseService.syncToMedusa();
+  }
+
   @Get(':id')
   findOne(@Param() params: CourseIdDto) {
     return this.courseService.findOne(params.id);
@@ -27,12 +33,7 @@ export class CourseController {
   }
 
   @Delete(':id')
-  remove(@Param() params: CourseIdDto) {
+  remove(@Param() params: StudentIdDto) {
     return this.courseService.remove(params.id);
-  }
-
-  @Get('sync/medusa')
-  syncToMedusa() {
-    return this.courseService.syncToMedusa();
   }
 }
